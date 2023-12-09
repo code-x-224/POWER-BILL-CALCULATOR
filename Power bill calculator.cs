92c8537201fa9ec61eae6f83d500b2582a274877
s1gm9 <113210463+Mathewsq369@users.noreply.github.com>
2023-12-10 02:07:42 +0300

    added loop to make it run multiple number of times

--- Power bill calculator.cs
+++ Power bill calculator.cs
@@ -5,88 +5,105 @@ class Program
 {
     static void Main()
     {
-     
+        bool running = true;
+        do
+        {
+            // initialization of the program
             Console.WriteLine("Welcome to the power billing system");
             Console.WriteLine("Below are the types of usage with their respective codes");
             Console.WriteLine("1 Residential");
             Console.WriteLine("2 Commercial");
             Console.WriteLine("3 Industrial");
-        int typeOfUsage;
-        decimal monthlyConsumption;
 
-        while (true)
-        {
+            //variables
+            int typeOfUsage;
+            decimal monthlyConsumption;
 
-            Console.WriteLine("Enter your code:");
-            string userInputTypeOfUsage = Console.ReadLine();
-
-            if (int.TryParse(userInputTypeOfUsage, out typeOfUsage) && typeOfUsage <= 3 && typeOfUsage > 0)
+            while (true)
             {
-                break;
+
+                Console.WriteLine("Enter your code:");
+                string userInputTypeOfUsage = Console.ReadLine();
+
+                if (int.TryParse(userInputTypeOfUsage, out typeOfUsage) && typeOfUsage <= 3 && typeOfUsage > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid code!!");
+                }
             }
-            else
+
+
+
+            while (true)
             {
-                Console.WriteLine("Enter a valid code!!");
+                Console.WriteLine("Enter your monthly consumption");
+                string userInputAmountOfUsage = Console.ReadLine();
+                if (decimal.TryParse(userInputAmountOfUsage, out monthlyConsumption))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid Monthly consumption input!!");
+                }
             }
-        }
 
-
-
-        while (true)
-        {
-            Console.WriteLine("Enter your monthly consumption");
-            string userInputAmountOfUsage = Console.ReadLine();
-            if (decimal.TryParse(userInputAmountOfUsage, out  monthlyConsumption))
+            static decimal ResidentialBillingAlgorithm(decimal monthlyConsumption)
             {
-                break;
+                decimal monthlyPowerBill = monthlyConsumption * 12.50m;
+                Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}kWh * 12.50 = {monthlyPowerBill}kes");
+                return monthlyPowerBill;
             }
-            else
+
+            static decimal CommercialBillingAlgorithm(decimal monthlyConsumption)
             {
-                Console.WriteLine("Enter a valid Monthly consumption input!!");
+                decimal monthlyPowerBill = monthlyConsumption * 15.75m;
+                Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}kWh * 15.75 = {monthlyPowerBill}kes");
+                return monthlyPowerBill;
             }
-        }
-        
-                        static decimal ResidentialBillingAlgorithm(decimal monthlyConsumption)
-                        {
-                            decimal monthlyPowerBill = monthlyConsumption * 12.50m;
-                            Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}kWh * 12.50 = {monthlyPowerBill}kes");
-                            return monthlyPowerBill;
-                        }
 
-                        static decimal CommercialBillingAlgorithm(decimal monthlyConsumption)
-                        {
-                            decimal monthlyPowerBill = monthlyConsumption * 15.75m;
-                            Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}kWh * 15.75 = {monthlyPowerBill}kes");
-                            return monthlyPowerBill;
-                        }
+            static decimal IndustrialBillingAlgorithm(decimal monthlyConsumption)
+            {
+                decimal monthlyPowerBill = monthlyConsumption * 18.90m;
+                Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}Kwh * 18.90 = {monthlyPowerBill}kes");
+                return monthlyPowerBill;
+            }
 
-                        static decimal IndustrialBillingAlgorithm(decimal monthlyConsumption)
-                        {
-                            decimal monthlyPowerBill = monthlyConsumption * 18.90m;
-                            Console.WriteLine($"Your monthly power bill is :{monthlyConsumption}Kwh * 18.90 = {monthlyPowerBill}kes");
-                            return monthlyPowerBill;
-                        }
+            if (typeOfUsage == 1)
+            {
+                ResidentialBillingAlgorithm(monthlyConsumption);
+            }
+            else if (typeOfUsage == 2)
+            {
+                CommercialBillingAlgorithm(monthlyConsumption);
+            }
+            else if (typeOfUsage == 3)
+            {
+                IndustrialBillingAlgorithm(monthlyConsumption);
+            }
 
-                        if (typeOfUsage == 1)
-                        {
-                            ResidentialBillingAlgorithm(monthlyConsumption);
-                        }
-                        else if (typeOfUsage == 2)
-                        {
-                            CommercialBillingAlgorithm(monthlyConsumption);
-                        }
-                        else if (typeOfUsage == 3)
-                        {
-                            IndustrialBillingAlgorithm(monthlyConsumption);
-                        }
+            Console.Write("Rerun power bill calculator?");
+            string run = Console.ReadLine();
+            bool proceed = true;
+
+            while(proceed)
+            {
+                switch (run.ToUpper())
+                case "Y" || "YES":
+                    proceed = false;
+                    break;
+                case "N" || "NO":
+                    running = false;
+                    proceed = false;
+                    break;
+                default:
+                    Console.WriteLine("Wrong input made!");
+                    break;
+            }
+        } while (running);
 
    }
 }
-
-
-
-
-
-
-
-
